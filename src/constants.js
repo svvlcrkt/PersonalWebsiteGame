471ff4f32327c6701c4f22699648f2fd3df200af
svvlc <svvlozlem.carkit@gmail.com>
2024-05-13 18:28:59 +0300

    first

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my computer, it supported me a lot during my university years :) 
+    I generally watch code trainings and do researchs here. I generally use Python to develop 
+    in the field of data science these days. I also have a medium account and I write articles there. 
+    You can check: https://svvlozlem-carkit.medium.com/`,
+    "cs-degree": `This is my CS degree. I graduated from Bahcesehir University in July 2023. 
+    I wanted to hang it here because it was an important turning point in my life :)`,
+    "sofa-table": `That's my sofa. I usually drink various kinds of coffee here :)`,
+    tv: `This is my TV, I usually use it to watch the daily news and the Bahar TV series :D`,
+    bed: `This is my bed, usually good ideas come to my mind while I'm just sleeping here. I'm implementing it right away :)`,
+    resume: `This is my desk and on it is my resume. <a href="" target="_blank">Check it out?</a>
+    Contact me at svvlozlem.carkit@gmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+    Kaboom.js by watching some of tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    library: `This is my library, there are many software and personal development books here. 
+    I also love reading comics. And my favorite author is Fabien Toulme :)`,
+    exit: `If you want to exit my portfolio, just close the tab.`,
+  }; 
